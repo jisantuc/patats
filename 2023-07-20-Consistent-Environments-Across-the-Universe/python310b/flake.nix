b9dbb0acd2aa7d67b6499b01d6e58726554f0d75
James Santucci <james.santucci@gmail.com>
2023-08-14 11:49:19 -0600

    Add nix + patat dev tools presentation (#7)

---
+++ 2023-07-20-Consistent-Environments-Across-the-Universe/python310b/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            (python310.withPackages (p: [ p.django ] ))
+          ];
+        };
+      });
+    };
+}
