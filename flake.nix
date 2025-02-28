4b5cba8404924a9eef5e1078ce02cd46b90a80b7
James Santucci <james.santucci@gmail.com>
2025-02-28 09:24:36 -0800

    back to bat

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
         devShells.default = pkgs.mkShell {
           name = "platform-dev-shell";
           buildInputs = with pkgs; [
+            bat
             entr
             haskellPackages.patat
           ];
